9513d5885d34abd463c3bbf46f225f7de0b12fa9
YunHo <imsong1997@gmail.com>
2024-08-27 21:15:54 +0900

    PrimaryKey 추가

--- data/src/main/java/com/yunho/data/local/TotalHourEntity.kt
+++ data/src/main/java/com/yunho/data/local/TotalHourEntity.kt
@@ -5,6 +5,8 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "MonthlyTotal")
 data class TotalHourEntity(
+    @PrimaryKey(autoGenerate = true)
+    val index: Long? = null,
     val year: Int? = 0, // 2024
     val month: Int? = null, // 7
     val minute: Int? = 0 //
